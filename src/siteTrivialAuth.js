660cf78048dee2667bbd165e555e4ce537826693
John Adney <jmoon5ftm@gmail.com>
2020-10-05 03:34:19 -0700

    housekeeping

--- src/siteTrivialAuth.js
+++ src/siteTrivialAuth.js
@@ -1,9 +1,9 @@
-export default (req, res, next) => {
+module.exports = (req, res, next) => {
   const token = process.env.TOKEN;
   const authMethod = req.get('Authorization');
 
   if (!authMethod || !authMethod.toLowerCase().startsWith("bearer ") || authMethod.substring(7).trim() !== token) {
-    res.status(401).json({ error: 'Unauthorized access, please provide proper authentication' });
+    res.status(401).json({ error: 'Unauthorized access; please provide proper authentication' });
   } else {
     next();
   }
